cdc217a81190c7d545043806650549d6ed41565b
root <root@YutoKawamura>
2023-11-05 17:45:17 +0900

    edited_dfwo

--- gptdemo/libs/exsample_selector.py
+++ gptdemo/libs/exsample_selector.py
@@ -32,6 +32,17 @@ def excel_to_qa_list(test_excel_path):
         qa_list.append(qa_single)
     return qa_list
 
+def edited_df_to_qa_list(edited_df):
+    df = edited_df
+    #データの変換
+    qa_list = []
+    for index, row in df.iterrows():
+        input_value = row[0]  # 2列目の値を取得
+        output_value = row[1]  # 3列目の値を取得
+        qa_single = {"input": input_value, "output": output_value}  # 指定の形式に変換
+        qa_list.append(qa_single)
+    return qa_list
+
 
 def get_qa(query,neighborNum):
     #定数設定
@@ -48,11 +59,53 @@ def get_qa(query,neighborNum):
     #プロンプトテンプレートの用意
     qa_prompt = PromptTemplate(
         input_variables=["input", "output"],
-        template="Q: {input}\nA: {output}",
+        template="Q: {input}\n\nA: {output}",
     )
 
     #エクセルの読み込みからデータの変換実施
     qa_list = excel_to_qa_list(test_excel_path)
+    
+    qa_selector = SemanticSimilarityExampleSelector.from_examples(
+    # This is the list of examples available to select from.
+    qa_list,
+    # This is the embedding class used to produce embeddings which are used to measure semantic similarity.
+    embedding_openai,
+    # This is the VectorStore class that is used to store the embeddings and do a similarity search over.
+    FAISS,
+    # This is the number of examples to produce.
+    k=neighborNum
+)
+    similar_prompt = FewShotPromptTemplate(
+    # We provide an ExampleSelector instead of examples.
+    example_selector=qa_selector,
+    example_prompt=qa_prompt,
+    prefix="■質問内容: {question}",
+    suffix="質問内容に近い{}件のQAを表示しました".format(neighborNum),
+    input_variables=["question"],
+)
+
+    return similar_prompt.format(question=query)
+
+def get_qa_edited_df(query,neighborNum,edited_df):
+    #定数設定
+    load_dotenv()
+    model_engine = os.environ["OPENAI_MODEL_ENGINE"]
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    #ファイル名から拡張子以外を取得
+    # filename = os.path.splitext(os.path.basename(test_excel_path))[0]
+
+    #OpenAIAPIで埋め込みの用意
+    embedding_openai = OpenAIEmbeddings()
+
+    #プロンプトテンプレートの用意
+    qa_prompt = PromptTemplate(
+        input_variables=["input", "output"],
+        template="Q: {input}\n\nA: {output}",
+    )
+
+    #エクセルの読み込みからデータの変換実施
+    qa_list = edited_df_to_qa_list(edited_df)
     
     qa_selector = SemanticSimilarityExampleSelector.from_examples(
     # This is the list of examples available to select from.
--- gptdemo/pages/04_(ベータ版)FAQ回答.py
+++ gptdemo/pages/04_(ベータ版)FAQ回答.py
@@ -15,6 +15,24 @@ from libs.exsample_selector import *
 #タイトルを表示
 st.title('🦜FAQ回答')
 
+st.markdown("""
+
+## 概要
+ユーザーの質問内容から意味検索を行い、最も意味的に近いQを探し出し、
+それに対応するAを回答します。
+意味的な検索のため、従来よりもより柔軟な検索ができます
+
+例
+「私の家でもなんとかなる？」という質問を投げかけると
+もっとも意味の近い
+「Q: 新型コロナウイルス感染症に罹った場合、自宅療養は可能ですか？」というQがヒットし
+そのAを返します
+
+## デモ用エクセル
+以下はデモ用に作成したコロナウイルス感染症FAQです。以下の表を直接操作して
+内容を追加/編集/削除することも可能です。
+""")
+
 df = pd.read_excel('gptdemo/data/qa/virusQA.xlsx',sheet_name='シート1',
         index_col='QAの番号')
 
@@ -23,7 +41,7 @@ df = pd.read_excel('gptdemo/data/qa/virusQA.xlsx',sheet_name='シート1',
 #     #     index_col='QAの番号')
 #     st.table(df)
 
-edited_df = st.data_editor(df)
+edited_df = st.data_editor(df,num_rows="dynamic")
 
 #最後に、st.form() を使用して、ユーザーが入力したプロンプトを受け入れるためのテキストボックス (st.text_area()) を作成します。
 #ユーザーが「送信」ボタンをクリックした時点で、prompt 入力変数 (テキスト) を引数として、generate-response() 関数が呼び出されます。
@@ -32,8 +50,9 @@ k = st.selectbox(label="回答数を選択",
 
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', '')
+  text = st.text_area('質問したい内容を入力:', '')
   submitted = st.form_submit_button('Submit')
   if submitted:
-    answer = get_qa(text,k)
+    # answer = get_qa(text,k)
+    answer = get_qa_edited_df(text,k,edited_df)
     st.info(answer)
