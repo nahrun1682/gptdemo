6fb041893e548b9c50be65343b548fb72a690411
nahrun1682 <kawamura100995@gmail.com>
2023-12-25 10:47:43 +0900

    クラウドコストUI変更

--- gptdemo/pages/14_クラウドコスト計算.py
+++ gptdemo/pages/14_クラウドコスト計算.py
@@ -1,24 +1,29 @@
 import streamlit as st
 import requests  # AWS Price List APIを叩くために使用します
 
+import streamlit as st
+
 # ストリームリットのタイトル
 st.title("AWS EC2 Cost Calculator")
 
-# ユーザー入力のためのセレクトボックスを設定
-instance_type = st.selectbox('Select EC2 Instance Type', ['t2.micro', 't2.small', 't2.medium'])
-region = st.selectbox('Select AWS Region', ['us-east-1', 'us-west-1'])
+# カラムの設定
+col1, col2 = st.columns(2)
+
+# カラム1にインスタンスタイプのセレクトボックスを配置
+with col1:
+    instance_type = st.selectbox('Select EC2 Instance Type', ['t2.micro', 't2.small', 't2.medium'])
+
+# カラム2にリージョンのセレクトボックスを配置
+with col2:
+    region = st.selectbox('Select AWS Region', ['us-east-1', 'us-west-1'])
 
 # 計算ボタン
 if st.button('Calculate Cost'):
-    # 実際には、AWS Pricing APIを呼び出して料金を取得する必要があります。
-    # ここでは、サンプルとして固定値を返すようにしています。
-    # AWS Price List APIから価格情報を取得する実装が必要です。
+    # 価格計算ロジック（サンプルのため固定値を使用）
     price_per_hour = 0.0116  # 例としてt2.microの価格を固定
+    monthly_cost = 24 * 30 * price_per_hour  # 月額コストの計算
 
-    # 簡易的なコスト計算
-    monthly_cost = 24 * 30 * price_per_hour
-
-    # 結果を表示
+    # 結果の表示
     st.write(f"Estimated monthly cost for {instance_type} in {region}: ${monthly_cost:.2f}")
 
 # 注意書き
